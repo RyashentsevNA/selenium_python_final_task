971910ec3792b391a697bccff92cc8211dc09d5c
Nikita Ryashentsev <RyashentsevNA@yandex.ru>
2024-10-17 23:12:05 +0300

    2

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
-from .pages.main_page import MainPage
+from selenium_python_final_task.pages.main_page import MainPage
+from selenium import webdriver
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,4 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
 
